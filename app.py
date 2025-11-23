fd045208e6b4d883a2a61f8dcaa4ed96b254df08
nsellis1500-cell <nsellis1500@gmail.com>
2025-11-23 18:40:28 -0500

    Add project folder and initial app.py with welcome message

--- app.py
+++ app.py
@@ -1,16 +1,2 @@
 # Task 1: Welcome message
 print("Welcome to the Savings Goal Tracker!")
-
-# Task 2: Ask the user for input
-# Collect:
-# - their name (optional)
-# - their total savings goal
-# - how much they have saved so far
-# - how much they plan to save each week
-
-name = input("What is your name? (Press Enter to skip) ")
-
-goal_input = input("What is your total savings goal in dollars? ")
-current_input = input("How much have you saved so far in dollars? ")
-weekly_input = input("How much do you plan to save each week in dollars? ")
-
