12bc9bf1c1ceaaf7aa2de54c179ad61e60b47510
nsellis1500-cell <nsellis1500@gmail.com>
2025-11-23 18:42:54 -0500

    Add user input prompt

--- app1.py
+++ app1.py
@@ -13,4 +13,3 @@ name = input("What is your name? (Press Enter to skip) ")
 goal_input = input("What is your total savings goal in dollars? ")
 current_input = input("How much have you saved so far in dollars? ")
 weekly_input = input("How much do you plan to save each week in dollars? ")
-
